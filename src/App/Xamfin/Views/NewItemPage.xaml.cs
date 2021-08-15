acdbb1b787ffaaba90276a58a35d74cfa950ee97
charleypeng <charleypeng@gmail.com>
2021-08-15 16:18:01 +0800

    change sdk from openapi client to official sdk client

---
+++ src/App/Xamfin/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using Xamfin.Models;
+using Xamfin.ViewModels;
+
+namespace Xamfin.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
--- src/Xamfin/Controls/PaddedScrollView.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using Xamarin.Forms;
-
-namespace Xamfin.Controls
-{
-    public class PaddedScrollView : ScrollView
-    {
-    }
-}
